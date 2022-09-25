0aab51ebd66ae49046caad2433f4bec86ea8941b
AkashMuhundhan0118 <113562234+AkashMuhundhan0118@users.noreply.github.com>
2022-09-25 13:44:44 -0400

    Exit Icon with Image Files

--- app/src/main/java/ca/future/home/it/secure/home/automation/MainActivity.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.future.home.it.secure.home.automation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onBackPressed() {
+
+        AlertDialog.Builder builder
+                = new AlertDialog
+                .Builder(MainActivity.this);
+
+        builder
+                .setMessage(R.string.leavingMsg)
+
+                .setTitle(R.string.leavingTitle)
+                .setIcon(R.drawable.exit_icon);
+        builder.setCancelable(false);
+
+        builder
+                .setPositiveButton(
+                        "Yes",
+                        (dialog, which) -> finish());
+
+        builder
+                .setNegativeButton(
+                        "No",
+                        (dialog, which) -> dialog.cancel());
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
