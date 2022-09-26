6e4e05f26929ce038ae1bb9ad06bb218797a4a2b
KrushangParekh5355 <krushang002@gmail.com>
2022-09-25 20:53:12 -0700

    added lights fragments

--- app/src/main/java/ca/future/home/it/secure/home/automation/HomeFragment.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/HomeFragment.java
@@ -3,7 +3,7 @@ Authors/Std.#/Section:
 Zachary Learoyd (LRDZ0002) - CENG-322-0NC
 Akash Muhundhan (N01420118) - CENG-322-0NA
 Harpreet Cheema (???) - CENG-322-???
-Krushang Parekh (???) - CENG-322-???
+Krushang Parekh (N01415355) - CENG-322-0NC
 */
 
 package ca.future.home.it.secure.home.automation;
