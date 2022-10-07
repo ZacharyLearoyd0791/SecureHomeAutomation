622b9f4a773e75db6d98ccdd72fbe1f779b84cc4
AkashMuhundhan0118 <113562234+AkashMuhundhan0118@users.noreply.github.com>
2022-10-06 22:14:14 -0400

    Crashlytics build gradle + test crash option

--- app/src/main/java/ca/future/home/it/secure/home/automation/MainActivity.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/MainActivity.java
@@ -8,13 +8,14 @@ Krushang Parekh (N01415355) - CENG-322-0NC
 
 package ca.future.home.it.secure.home.automation;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
@@ -33,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+Button crashButton = new Button(this);
+crashButton.setText("Test Crash");
+crashButton.setOnClickListener(new View.OnClickListener() {
+   public void onClick(View view) {
+       throw new RuntimeException("Test Crash"); // Force a crash
+   }
+});
+
+addContentView(crashButton, new ViewGroup.LayoutParams(
+       ViewGroup.LayoutParams.MATCH_PARENT,
+       ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+
+
+
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         homeFragment = new HomeFragment();
