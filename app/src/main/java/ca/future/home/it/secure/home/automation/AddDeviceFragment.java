4c27e04cd1c1f4d4a56cffacda041000286146a9
ZacharyLearoyd0791 <zachary.learoyd@gmail.com>
2022-10-11 17:39:46 -0400

    -Updated and added comments on all java classes, strings.xml and themes.xml -Updated .gitignore file, including added names as a comment

--- app/src/main/java/ca/future/home/it/secure/home/automation/AddDeviceFragment.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/AddDeviceFragment.java
@@ -2,7 +2,7 @@
 Authors/Std.#/Section:
 Zachary Learoyd (LRDZ0002) - CENG-322-0NC
 Akash Muhundhan (N01420118) - CENG-322-0NA
-Harpreet Cheema (???) - CENG-322-0NA
+Harpreet Cheema (N01438638) - CENG-322-0NA
 Krushang Parekh (N01415355) - CENG-322-0NC*/
 package ca.future.home.it.secure.home.automation;
 
