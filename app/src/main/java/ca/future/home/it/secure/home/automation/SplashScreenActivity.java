e05764cdc50094f625a1f27f3a508f61596ab01a
HarpreetCheema8638 <terapeyo21@gmail.com>
2022-10-11 09:58:35 +0500

    added splash screen layout for both orientations and updated main activity layout and fragment

--- app/src/main/java/ca/future/home/it/secure/home/automation/SplashScreenActivity.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/SplashScreenActivity.java
@@ -19,7 +19,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
