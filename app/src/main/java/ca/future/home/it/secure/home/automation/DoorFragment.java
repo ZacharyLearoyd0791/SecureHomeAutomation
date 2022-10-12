6600b251b7d202ed5fb3dbedb9cec38ce7d833ec
AkashMuhundhan0118 <113562234+AkashMuhundhan0118@users.noreply.github.com>
2022-10-11 21:38:45 -0400

    New Door Feature

--- app/src/main/java/ca/future/home/it/secure/home/automation/DoorFragment.java
+++ app/src/main/java/ca/future/home/it/secure/home/automation/DoorFragment.java
@@ -8,6 +8,8 @@ Krushang Parekh (N01415355) - CENG-322-0NC
 
 package ca.future.home.it.secure.home.automation;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,14 +53,17 @@ public class DoorFragment extends Fragment {
                 Toast.makeText(getActivity(), R.string.openDoor, Toast.LENGTH_SHORT).show();
                 doorLock.setBackgroundResource(R.drawable.ic_lock);
                 status.setText(R.string.unlock);
-
-
+                doorLock.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF0000")));
+                doorLock.setTextOff(getString(R.string.space));
+                doorLock.setTextOn(getString(R.string.space));
             } else {
                 doorLock.setBackgroundColor(android.graphics.Color.parseColor("#FF0000"));
                 Toast.makeText(getActivity(), R.string.closedDoor, Toast.LENGTH_SHORT).show();
                 doorLock.setBackgroundResource(R.drawable.ic_lock_closed);
                 status.setText(R.string.lock);
-
+                doorLock.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#1AFF00")));
+                doorLock.setTextOff(getString(R.string.space));
+                doorLock.setTextOn(getString(R.string.space));
             }
         });
         }
