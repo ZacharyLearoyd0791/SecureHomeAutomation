249b58c27b6b27ab757df196f73f8c64f3172f40
HarpreetCheema8638 <98668791+HarpreetCheema8638@users.noreply.github.com>
2022-11-09 02:09:33 +0500

    Refactored some ids and initialized the UI objects in temperature fragment and added click listeners

--- SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/TempFragment.java
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/TempFragment.java
@@ -9,14 +9,20 @@ package ca.future.home.it.secure.home.automation;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TempFragment extends Fragment {
 
+    TextView tvCurrentTemperature;
+    TextView tvMinimumTemperature;
+    TextView tvMaximumTemperature;
     public TempFragment() {
         // Required empty public constructor
     }
@@ -26,5 +32,34 @@ public class TempFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_temp, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        setListeners();
+    }
+
+    private void init(View view) {
+        tvCurrentTemperature = view.findViewById(R.id.CurrentTemp);
+        tvMinimumTemperature = view.findViewById(R.id.MaximumTemperature);
+        tvMaximumTemperature = view.findViewById(R.id.MaximumTemperature);
+    }
+
+    private void setListeners() {
+        tvMaximumTemperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        tvMaximumTemperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
