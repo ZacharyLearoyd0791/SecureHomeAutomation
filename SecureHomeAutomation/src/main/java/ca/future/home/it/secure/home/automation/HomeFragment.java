46af53a9853e77a957e29a1dcad8e12e4baebc09
AkashMuhundhan0118 <113562234+AkashMuhundhan0118@users.noreply.github.com>
2022-11-08 21:09:12 -0500

    Code Clean up

--- SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/HomeFragment.java
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/HomeFragment.java
@@ -8,13 +8,12 @@ Krushang Parekh (N01415355) - CENG-322-0NC
 
 package ca.future.home.it.secure.home.automation;
 
-import android.net.Uri;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
@@ -33,31 +32,22 @@ public class HomeFragment extends Fragment {
     public View view;
 
     //Switches
-
-    public Switch lockSwitch;
-    public Switch tempSwitch;
-    public Switch lightSwitch;
-    public Switch windowSwitch;
+    Switch lockSwitch,tempSwitch,lightSwitch, windowSwitch;
 
     //Buttons
-    public ImageButton lockBtn;
-    public ImageButton tempBtn;
-    public ImageButton lightBtn;
-    public ImageButton windowBtn;
+    ImageButton lockBtn,tempBtn,lightBtn,windowBtn;
 
     //Text View
-    public TextView greetingsText;
-    public TextView quickAcc;
+    TextView greetingsText;
 
     //ImageView
-    public ImageView doorView;
-    public ImageView tempView;
-    public ImageView lightView;
-    public ImageView windowView;
-    public ImageView pressLock;
-    public ImageView pressTemp;
-    public ImageView pressLight;
+    ImageView doorView,tempView,lightView, windowView,pressLock,pressTemp,pressLight;
 
+    GoogleSignInAccount acct;
+
+    Date date;
+    Calendar cal;
+    int hour;
     final Handler handler = new Handler();
 
 
@@ -69,50 +59,14 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        acct = GoogleSignIn.getLastSignedInAccount(requireActivity());
+        init();
+        date();
+        greetings();
         return view;
     }
 
-    @Override
-    public void onViewCreated (View view, Bundle savedInstanceState){
-
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        int hour = cal.get(Calendar.HOUR_OF_DAY);
-
-
-        greetingsText = view.findViewById(R.id.Greetings);
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getActivity());
-        greetingsText.setText(null);
-
-
-        if (hour >= 6 && hour < 12) {
-
-            greetingsText.setText(R.string.greetingMorning);
-            view.setBackgroundResource(R.drawable.morning);
-
-
-        } else if (hour >= 12 && hour < 17) {
-
-            greetingsText.setText(R.string.greetingAfternoon);
-            view.setBackgroundResource(R.drawable.afternoon);
-
-        } else if (hour >= 17 && hour < 21) {
-
-            greetingsText.setText(R.string.greetingEvening);
-            view.setBackgroundResource(R.drawable.evening);
-
-        } else {
-
-            greetingsText.setText(R.string.greetingNight);
-            view.setBackgroundResource(R.drawable.night);
-
-        }
-
-
-
-
+    private void init() {
         //Switches
         lockSwitch = view.findViewById(R.id.sw_lock);
         tempSwitch = view.findViewById(R.id.sw_temp);
@@ -129,77 +83,74 @@ public class HomeFragment extends Fragment {
         pressLock = view.findViewById(R.id.iv_press_lock);
         pressTemp = view.findViewById(R.id.iv_press_temp);
         pressLight = view.findViewById(R.id.iv_press_light);
+        greetingsText = view.findViewById(R.id.Greetings);
+
+    }
+    private void date(){
+        date= new Date();
+        cal = Calendar.getInstance();
+        cal.setTime(date);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+    }
+
+    private void greetings(){
+        greetingsText.setText(null);
+
+        if (hour >= 6 && hour < 12) {
+            greetingsText.setText(R.string.greetingMorning);
+        }
+        else if (hour >= 12 && hour < 17) {
+            greetingsText.setText(R.string.greetingAfternoon);
+        }
+        else if (hour >= 17 && hour < 21) {
+            greetingsText.setText(R.string.greetingEvening);
+        }
+        else {
+            greetingsText.setText(R.string.greetingNight);
+        }
+    }
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState){
 
         pressLock.setVisibility(View.INVISIBLE);
         pressTemp.setVisibility(View.INVISIBLE);
         pressLight.setVisibility(View.INVISIBLE);
 
         //Lock button
-        lockBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                lockBtn.setVisibility(View.INVISIBLE);
-                pressLock.setVisibility(View.VISIBLE);
+        lockBtn.setOnClickListener(view14 -> {
+            lockBtn.setVisibility(View.INVISIBLE);
+            pressLock.setVisibility(View.VISIBLE);
 
-                //simulate button pressed
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.doorFragment).commit();
-                    }
-                }, 300);
-                //MainActivity.bottomNav.setSelectedItemId(R.id.door);
-            }
+            //simulate button pressed
+            handler.postDelayed(() -> getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.doorFragment).commit(), 300);
+            //MainActivity.bottomNav.setSelectedItemId(R.id.door);
         });
 
         //Temperature button
-        tempBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                tempBtn.setVisibility(View.INVISIBLE);
-                pressTemp.setVisibility(View.VISIBLE);
+        tempBtn.setOnClickListener(view13 -> {
+        tempBtn.setVisibility(View.INVISIBLE);
+        pressTemp.setVisibility(View.VISIBLE);
 
-                //simulate button pressed
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.tempFragment).commit();
-                    }
-                }, 300);
-                //MainActivity.bottomNav.setSelectedItemId(R.id.temp);
-            }
+            //simulate button pressed
+            handler.postDelayed(() -> getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.tempFragment).commit(), 300);
+            //MainActivity.bottomNav.setSelectedItemId(R.id.temp);
         });
 
         //Light button
-        lightBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                lightBtn.setVisibility(View.INVISIBLE);
-                pressLight.setVisibility(View.VISIBLE);
+        lightBtn.setOnClickListener(view12 -> {
+            lightBtn.setVisibility(View.INVISIBLE);
+            pressLight.setVisibility(View.VISIBLE);
 
-                //simulate button pressed
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.lightFragment).commit();
-                    }
-                }, 300);
+            //simulate button pressed
+            handler.postDelayed(() -> getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.lightFragment).commit(), 300);
 
-                //MainActivity.bottomNav.setSelectedItemId(R.id.light);
-            }
+            //MainActivity.bottomNav.setSelectedItemId(R.id.light);
         });
 
         //Window button
-        windowBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.windowFragment).commit();
-                //MainActivity.bottomNav.setSelectedItemId(R.id.window);
-            }
+        windowBtn.setOnClickListener(view1 -> {
+            getParentFragmentManager().beginTransaction().replace(R.id.flFragment, MainActivity.windowFragment).commit();
+            //MainActivity.bottomNav.setSelectedItemId(R.id.window);
         });
 
         //ImageView Objects
@@ -210,43 +161,35 @@ public class HomeFragment extends Fragment {
 
         //Switches selected
         //lock switch
-        lockSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    doorView.setVisibility(View.INVISIBLE);
-                } else {
-                    doorView.setVisibility(View.VISIBLE);
-                }
+        lockSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                doorView.setVisibility(View.INVISIBLE);
+            } else {
+                doorView.setVisibility(View.VISIBLE);
             }
         });
         //temperature switch
-        tempSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    tempView.setVisibility(View.INVISIBLE);
-                } else {
-                    tempView.setVisibility(View.VISIBLE);
-                }
+        tempSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tempView.setVisibility(View.INVISIBLE);
+            } else {
+                tempView.setVisibility(View.VISIBLE);
             }
         });
         //light switch
-        lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    lightView.setVisibility(View.INVISIBLE);
-                } else {
-                    lightView.setVisibility(View.VISIBLE);
-                }
+        lightSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                lightView.setVisibility(View.INVISIBLE);
+            } else {
+                lightView.setVisibility(View.VISIBLE);
             }
         });
         //window switch
-        windowSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    windowView.setVisibility(View.INVISIBLE);
-                } else {
-                    windowView.setVisibility(View.VISIBLE);
-                }
+        windowSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                windowView.setVisibility(View.INVISIBLE);
+            } else {
+                windowView.setVisibility(View.VISIBLE);
             }
         });
     }
