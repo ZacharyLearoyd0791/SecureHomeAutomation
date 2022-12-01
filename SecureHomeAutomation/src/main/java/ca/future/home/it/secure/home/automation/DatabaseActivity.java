e29b97b0baa8aa649661dacee2f2f988006259ae
AkashMuhundhan0118 <n01420118@humbermail.ca>
2022-11-30 20:37:27 -0500

    DatabaseActivity gets custom keys for Door and Light database info

--- SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/DatabaseActivity.java
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/DatabaseActivity.java
@@ -9,10 +9,12 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +27,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DatabaseActivity extends AppCompatActivity {
+public class DatabaseActivity extends Fragment {
 
     //Database
     FirebaseDatabase firebaseDatabase;
@@ -39,13 +41,33 @@ public class DatabaseActivity extends AppCompatActivity {
     DateFormat dateFormat;
 
     //String
-    String idKey,localKey,key,personalKey,strDate;
+    String idKey,localKey,key,personalKey,strDate,sensorKey,lightKey,statusKey,SensorKey,doorKey;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        initV();
+        dbID();
+        time();
+
+
+    }
+
+    private void initV() {
+        time();
+
+        Log.d(TAG,"Key init");
+        doorKey=getApplicationContext().getString(R.string.forwardslash)+getApplicationContext().getString(R.string.door_status)+getApplicationContext().getString(R.string.forwardslash)+strDate;
+        statusKey=getApplicationContext().getString(R.string.statusKey);
+        SensorKey=getApplicationContext().getString(R.string.db_ultrasonic_dist);
+        Log.d(TAG,"Key tester\n"+statusKey+"\n"+SensorKey);
+    }
 
     //FOR DOOR LOCK
     public void toDatabase(String status){
         DatabaseActivity.context = getApplicationContext();
-
-        dbID();
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference().child((idKey));
 
@@ -56,11 +78,9 @@ public class DatabaseActivity extends AppCompatActivity {
         databaseReference.updateChildren(updateStatus);
     }
 
-    private void dbID(){
+    public void dbID(){
         userInfo.typeAccount();
-        time();
-        //Log.d(TAG,"Time string before key;"+strDate);
-
+        initV();
         localKey=userInfo.userId;
         personalKey=userInfo.idInfo;
 
@@ -74,19 +94,25 @@ public class DatabaseActivity extends AppCompatActivity {
             Log.d(TAG, key);
         }
 
-        idKey=key+getString(R.string.forwardslash)+getString(R.string.door_status)+getString(R.string.forwardslash)+strDate;
+        idKey=key+doorKey;
+        lightKey=key+statusKey;
+
+        sensorKey=key+SensorKey;
+        Log.d(TAG,"Key grabbed from DatabaseClass: \n"+idKey+"\n"+lightKey+"\n"+sensorKey);
     }
 
-    @SuppressLint("SimpleDateFormat")
+   // @SuppressLint("SimpleDateFormat")
     private void time(){
+        Log.d(TAG,"STR TIME METHOD");
         date = Calendar.getInstance().getTime();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            dateFormat = new SimpleDateFormat(getString(R.string.formatted));
+            dateFormat = new SimpleDateFormat(getApplicationContext().getString(R.string.formatted));
         }
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             strDate = dateFormat.format(date);
         }
         System.out.println("Converted String: " + strDate);
+        Log.d(TAG,"STR Time: "+strDate);
     }
 }
