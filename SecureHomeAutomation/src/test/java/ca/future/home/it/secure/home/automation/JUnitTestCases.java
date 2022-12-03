cb23f415f042305b0c43fe467f07c05be66e157d
AkashMuhundhan0118 <n01420118@humbermail.ca>
2022-12-03 03:06:17 -0500

    Test case 1 with Junit4

---
+++ SecureHomeAutomation/src/test/java/ca/future/home/it/secure/home/automation/JUnitTestCases.java
@@ -0,0 +1,37 @@
+package ca.future.home.it.secure.home.automation;
+
+import androidx.test.filters.MediumTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+@MediumTest
+public class JUnitTestCases {
+    int sum;
+    String compare;
+    public void compare(String str1, String str2){
+        if (str1.equals(str2)){
+            compare="Same";
+        }
+        else{
+            compare="Not Same";
+        }
+
+
+    }
+    public int sum(int num1,int num2){
+        sum=num1+num2;
+        return sum;
+    }
+    @Test
+    public void testA() {
+
+        compare("Same","Same");
+        Assert.assertEquals(compare,"Same");
+        compare("Same","Different");
+        Assert.assertEquals(compare,"Not Same");
+    }
+
+
+}
