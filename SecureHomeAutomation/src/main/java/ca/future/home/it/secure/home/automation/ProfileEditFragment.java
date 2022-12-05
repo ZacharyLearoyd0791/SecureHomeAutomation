cf23cbb1e7cbd6c15af87660f6a0ddcfdaf88dde
AkashMuhundhan0118 <n01420118@humbermail.ca>
2022-12-05 11:46:54 -0500

    ProfileEditFragment.java hardcode into strings

--- SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/ProfileEditFragment.java
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/ProfileEditFragment.java
@@ -106,7 +106,7 @@ public class ProfileEditFragment extends Fragment {
         saveChanges.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Changes Saved!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), R.string.changesSaved, Toast.LENGTH_SHORT).show();
             }
         });
 
