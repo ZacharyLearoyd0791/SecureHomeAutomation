a998556690b23083a5f9fec8f0cd552394196f97
krushang <krushang002@gmail.com>
2023-04-04 18:40:44 -0400

    Made profile fragment editable

--- SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/AccountFragment.java
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/AccountFragment.java
@@ -11,63 +11,49 @@ import static android.content.Context.MODE_PRIVATE;
 
 import static com.facebook.FacebookSdk.getApplicationContext;
 
-import android.animation.Animator;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.airbnb.lottie.LottieAnimationView;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-public class AccountFragment extends Fragment {
+public class AccountFragment extends Fragment implements AccountRecyclerViewInterface {
     View view;
+
 
     //RecyclerView
     RecyclerView recyclerView;
+    String userChangedName;
     RecyclerView recyclerViewOthers;
     private List<AccountFragmentData> accountFragmentDataList;
     String userName;
@@ -173,7 +159,7 @@ public class AccountFragment extends Fragment {
         accountFragmentDataList.add(new AccountFragmentData(R.drawable.phone_icon_account_4,"Phone","Null"));
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),
                 DividerItemDecoration.VERTICAL));
-        adapter = new AccountFragmentRecyclerViewAdapter(accountFragmentDataList);
+        adapter = new AccountFragmentRecyclerViewAdapter(accountFragmentDataList,this);
         recyclerView.setAdapter(adapter);
 
 
@@ -232,4 +218,52 @@ public class AccountFragment extends Fragment {
 
         return userKey+profileKey;
     }
+
+
+    @Override
+    public void onItemClick(int position) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        final EditText alertEditText = new EditText(getApplicationContext());
+        if(position == 0) {
+            alertEditText.setText(userName);
+            alert.setMessage("Edit your name");
+            alert.setTitle("Change Name");
+            alert.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    userName = alertEditText.getText().toString();
+                    userNameTV.setText(userName);
+                }
+            });
+        }else if(position == 1){
+            alertEditText.setText(userEmail);
+            alert.setMessage("Edit your email");
+            alert.setTitle("Change Email");
+            alert.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    userEmail = alertEditText.getText().toString();
+
+                }
+            });
+        } else if (position == 2) {
+            alertEditText.setText(userPhone);
+            alert.setMessage("Edit your phone number");
+            alert.setTitle("Change Name");
+            alert.setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    userPhone = alertEditText.getText().toString();
+
+                }
+            });
+        }
+        alert.setView(alertEditText);
+        alert.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // If user click no then dialog box is canceled.
+            dialog.cancel();
+        });
+        AlertDialog alertDialog = alert.create();
+        alertDialog.show();
     }
+}
---
+++ SecureHomeAutomation/src/main/java/ca/future/home/it/secure/home/automation/AccountRecyclerViewInterface.java
@@ -0,0 +1,5 @@
+package ca.future.home.it.secure.home.automation;
+
+ interface AccountRecyclerViewInterface {
+     void onItemClick(int position);
+ }
